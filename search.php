7f3f1fd6d58e2ebab385ff3ada182fa9e3d08e74
Unknown <pavel.panaitov@gmail.com>
2018-01-02 17:37:23 +0200

    Search page was added

--- search.php
+++ search.php
@@ -15,27 +15,29 @@ get_header(); ?>
         <?php
         if(have_posts()) : ?>
 
-					<header class="page-header">
-						<h1 class="page-title"><?php
-              /* translators: %s: search query. */
-              printf(esc_html__('Search Results for: %s', 'stormguard'), '<span>' . get_search_query() . '</span>');
-              ?></h1>
-					</header><!-- .page-header -->
+					<div class="row">
+            <?php
+            /* Start the Loop */
+            while (have_posts()) : the_post();
+
+              /**
+               * Run the loop for the search to output the results.
+               * If you want to overload this in a child theme then include a file
+               * called content-search.php and that will be used instead.
+               */
+              get_template_part('template-parts/content', 'search');
+
+            endwhile; ?>
+					</div>
+					<!-- /.row -->
 
           <?php
-          /* Start the Loop */
-          while (have_posts()) : the_post();
-
-            /**
-             * Run the loop for the search to output the results.
-             * If you want to overload this in a child theme then include a file
-             * called content-search.php and that will be used instead.
-             */
-            get_template_part('template-parts/content', 'search');
-
-          endwhile;
-
-          the_posts_navigation();
+          if(function_exists('wp_pagenavi')) : ?>
+						<div class="pagination">
+              <?php wp_pagenavi(); ?>
+						</div>
+						<!-- /.pagination -->
+          <?php endif;
 
         else :
 
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -7,26 +7,4 @@
  * @package stormguard
  */
 
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php stormguard_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php stormguard_post_thumbnail(); ?>
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php stormguard_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+get_template_part( 'template-parts/content', 'page' );
