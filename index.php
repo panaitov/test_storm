aeb9cebe6499325ce461bd269888fed412f4bff1
Unknown <pavel.panaitov@gmail.com>
2018-01-02 09:21:01 +0200

    Blog and search pages were added

--- index.php
+++ index.php
@@ -17,42 +17,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="container">
-			<?php
-			if ( have_posts() ) :
+        <?php
+        if(have_posts()) :
 
-				if ( is_home() && ! is_front_page() ) : ?>
-					<header>
-						<h3 class="page-title screen-reader-text"><?php single_post_title(); ?></h3>
-					</header>
+          /* Start the Loop */
+          while (have_posts()) : the_post();
 
-				<?php
-				endif;
+            /*
+             * Include the Post-Format-specific template for the content.
+             * If you want to override this in a child theme, then include a file
+             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+             */
+            get_template_part('template-parts/content', 'page');
 
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+          endwhile;
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
+        else :
 
-				endwhile;
+          get_template_part('template-parts/content', 'none');
 
-			else :
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif; ?>
+        endif;
+        get_template_part('template-parts/content', 'services_cat'); ?>
 
 			</div>
 			<!-- /.container -->
+      <?php get_template_part('template-parts/franchise', 'form'); ?>
 
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
